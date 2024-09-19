bc454cb494e6631b3b587a79d08f8b84a430f0d0
Jan Mjartan <mjartan.jan@gmail.com>
2024-09-19 20:05:42 +0200

    apply timeout to other monitors

--- src/main/kotlin/life/vaporized/servermonitor/app/monitor/resources/BashNumberResourceMonitor.kt
+++ src/main/kotlin/life/vaporized/servermonitor/app/monitor/resources/BashNumberResourceMonitor.kt
@@ -4,6 +4,7 @@ import life.vaporized.servermonitor.app.monitor.IResourceMonitor
 import life.vaporized.servermonitor.app.monitor.model.MonitorStatus
 import life.vaporized.servermonitor.app.monitor.model.NumberResourceDefinition
 import life.vaporized.servermonitor.app.util.getLogger
+import java.util.concurrent.TimeUnit
 
 /**
  * Executes a command yielding int value that can be shown in a graph
@@ -19,24 +20,28 @@ open class BashNumberResourceMonitor(
     override val message: String = command.description
 
     override suspend fun evaluate(): List<MonitorStatus.ResourceStatus> =
-        MonitorStatus.ResourceStatus(
-            id = id,
-            name = name,
-            description = message,
-            current = getResourceValue(),
-            total = 100f,
-        ).let { listOf(it) }
+        listOfNotNull(getResourceValue()).map {
+            MonitorStatus.ResourceStatus(
+                id = id,
+                name = name,
+                description = message,
+                current = it,
+                total = 100f,
+            )
+        }
 
-    private fun getResourceValue(): Float {
+    private fun getResourceValue(): Float? {
         try {
             val process = ProcessBuilder(*command.command.toTypedArray())
                 .apply {
                     environment().putAll(System.getenv())
                 }.start()
 
+            process.waitFor(20, TimeUnit.SECONDS)
+
             val processResult = process.inputStream.bufferedReader().use { reader ->
                 val value = reader.readText().trim()
-                value.toFloatOrNull() ?: -1f
+                value.toFloatOrNull()
             }
 
             val errors = process.errorStream.bufferedReader().readText()
